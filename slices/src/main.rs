cacb920831264d07d3bc203bb0902cc74b69fd91
Luciano Correia <luciano.correia@gmail.com>
2021-10-06 21:28:29 -0300

    slices

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {}
+fn main() {
+    let my_string = String::From("hello world");
 
-fn first_word(s: &String) -> usize {}
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+    let word = first_word(&my_string);
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
+}
