1b3432a86784730151bf8571da12e5748098e05c
Luciano Correia <luciano.correia@gmail.com>
2021-10-08 19:53:29 -0300

    generics

---
+++ generics_traits_lifetime/src/main.rs
@@ -0,0 +1,35 @@
+// fn largest<T>(list: &[T]) -> T {
+//     let mut largest = list[0];
+
+//     for &item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+
+//     largest
+// }
+
+// fn main() {
+//     let number_list = vec![34, 50, 25, 100, 65];
+
+//     let result = largest(&number_list);
+//     println!("The largest number is {}", result);
+//     assert_eq!(result, 100);
+
+//     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+//     let result = largest(&number_list);
+//     println!("The largest number is {}", result);
+//     assert_eq!(result, 6000);
+// }
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+fn main() {
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+}
