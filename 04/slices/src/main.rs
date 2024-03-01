31a5c5491f72b357beb8f2ba5989f3d8cc6dba9a
Luciano Correia <luciano.correia@gmail.com>
2024-03-01 20:06:05 +0400

    04 - slices

---
+++ 04/slices/src/main.rs
@@ -0,0 +1,40 @@
+#![allow(unused_variables)]
+
+fn main() {
+    let my_string = String::from("hello world");
+
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+
+    let word = first_word(&my_string);
+
+    let my_string_literal = "hello world";
+
+    // `first_word` works on slices of string literals, whether partial or whole
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word(my_string_literal);
+
+    println!("The first word is: {}", word);
+
+    let a = [1, 2, 3, 4, 5];
+    let slice = &a[1..3];
+
+    assert_eq!(slice, &[2]);
+}
+
+// first word function
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    s
+}
