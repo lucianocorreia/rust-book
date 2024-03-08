edc811f2d7a8c74cc2a411ebecf8ae4ebc060e76
Luciano Correia <luciano.correia@gmail.com>
2024-03-08 20:47:35 +0400

    12 - minigrep

---
+++ minigrep/src/main.rs
@@ -0,0 +1,26 @@
+#![allow(unused)]
+
+use std::{
+    env::{args, Args},
+    error::Error,
+    fs::read_to_string,
+    process::exit,
+};
+
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        exit(1);
+    });
+
+    println!("-> Searching for {}", config.query);
+    println!("-> In file {}\n", config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        exit(1);
+    }
+}
